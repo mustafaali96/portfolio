a0ec2339064ec059917b1160c572a8ebe6ec46d4
mustafaali96 <ssuet.se2016@gmail.com>
2021-03-10 10:40:45 +0500

    views render url

--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request, 'custom_app/index.html')
+    return render(request, 'index.html')
 
 def experience_view(request):
-    return render(request, 'custom_app/index.html')
+    return render(request, 'experience.html')
 
 def projects_view(request):
-    return render(request, 'custom_app/index.html')
+    return render(request, 'projects.html')
 
 def certification_view(request):
-    return render(request, 'custom_app/index.html')
+    return render(request, 'certification.html')
 
 def research_view(request):
-    return render(request, 'custom_app/index.html')
+    return render(request, 'research.html')
 
 def education_view(request):
-    return render(request, 'custom_app/index.html')
+    return render(request, 'education.html')
