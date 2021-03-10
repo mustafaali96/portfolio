91134eaea6748a20bbbd6a85e133a22c22457d67
mustafaali96 <ssuet.se2016@gmail.com>
2021-03-10 10:53:25 +0500

    research static

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls, name = "admin"),
     path('', views.index_view, name= "home"),
     path('Experience/', views.experience_view, name= "experience"),
-    path('Projects', views.projects_view, name= "projects"),
+    path('Projects/', views.projects_view, name= "projects"),
     path('Certification/', views.certification_view, name= "certification"),
-    path('Research', views.research_view, name= "research"),
-    path('Education', views.education_view, name= "education"),
+    path('Research/', views.research_view, name= "research"),
+    path('Education/', views.education_view, name= "education"),
 ]
